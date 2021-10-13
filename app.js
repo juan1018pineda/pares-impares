f129570572ab47dfb60c238698dd8d91e21cdc60
pineda.juan92 <pineda.juan92@gmail.com>
2021-10-13 18:35:15 -0500

    Pares e impares solution, Make It Real challenge

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    let html = '';
+    let p = '';
+    for (let i = 1; i<= 50; i++){
+        if(i % 2 === 0){
+            p = `<p>${i} soy par!</p>`;
+            html += p;
+        } else {
+            p = `<p>${i} soy impar!</p>`;
+            html += p;
+        }
+    }
+    res.send(html);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000!"));
